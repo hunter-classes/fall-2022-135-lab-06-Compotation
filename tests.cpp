a515f3ed8705daf18d2a76ba4d10bf39d24e65f3
Compotation <46853874+Compotation@users.noreply.github.com>
2022-10-13 14:44:12 -0400

    tests for task b

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,25 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "funcs.h"
+#include "caesar.h"
 
-// add your tests here
+TEST_CASE("task b") {
+    CHECK(shiftChar(';', 1) == ';');
+    CHECK(shiftChar('.', 20) == '.');
+    CHECK(shiftChar('a', 1) == 'b');
+    CHECK(shiftChar('a', 25) == 'z');
+    CHECK(shiftChar('A', 1) == 'B');
+    CHECK(shiftChar('A', 25) == 'Z');
+    CHECK(shiftChar('a', 0) == 'a');
+    CHECK(shiftChar('z', 0) == 'z');
+    CHECK(shiftChar('A', 0) == 'A');
+    CHECK(shiftChar('z', 2) == 'b');
+    CHECK(shiftChar('z', 25) == 'y');
+    CHECK(shiftChar('Z', 3) == 'C');
+    CHECK(shiftChar('Z', 25) == 'Y');
+
+    CHECK(encryptCaesar("Way to Go!", 5) == "Bfd yt Lt!");
+    CHECK(encryptCaesar("!!!!...____", 20) == "!!!!...____");
+    CHECK(encryptCaesar("This is C11", 8) == "Bpqa qa K11");
+    CHECK(encryptCaesar("THIS IS PYTHON.", 7) == "AOPZ PZ WFAOVU.");
+    CHECK(encryptCaesar("aoweif.c,oijafwoieefawe", 24) == "ymucgd.a,mghydumgccdyuc");
+}
